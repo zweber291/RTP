3c9a987f0eb43ae3fc9b5db51af499ff9155846d
Zach <zweebees@gmail.com>
2020-01-15 00:31:13 -0500

    Add config
    Added config and renamed package. Config value "distance" will control how far the player teleports (smaller values = less performance hit)

--- RTP/src/com/smack17/rtp/Main.java
+++ RTP/src/com/smack17/rtp/Main.java
@@ -1,12 +1,20 @@
-package com.zweber.rtp;
+package com.smack17.rtp;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+	FileConfiguration config = this.getConfig();
+	public static Main plugin;
+	
     public void onEnable() {
+    	plugin = this;
         getLogger().info("Enabling RTP"); // Sends message to console saying the plugin was enabled
         getCommand("rtp").setExecutor(new commandRtp()); // Associates /rtp command with the commandRtp class
+        config.addDefault("distance", 1000); // Creates configuration file to configure distance teleported
+        config.options().copyDefaults(true); // Copies values to config file
+        saveConfig(); // Saves config
     }
 
     public void onDisable() {
