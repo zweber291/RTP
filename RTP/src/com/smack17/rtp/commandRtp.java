61ae573ccf41bc6b0695715470253b25c3757ad3
Zach <zweebees@gmail.com>
2020-01-15 13:43:20 -0500

    Load chunk before checking for water
    Thought the issue might stem from a chunk not being generated, therefor it cant check for water. Will test this theory.

--- RTP/src/com/smack17/rtp/commandRtp.java
+++ RTP/src/com/smack17/rtp/commandRtp.java
@@ -27,32 +27,42 @@ public class commandRtp implements CommandExecutor {
         // Choose what direction player will tp in and check if it is water. If so, will pick a new location
         switch(posOrNeg) {
         case 1:
+        	World worldPP = locPlusPlus.getWorld();
+        	worldPP.getChunkAt((int)locPlusPlus.getX(), (int)locPlusPlus.getZ()).load();
         	while(locPlusPlus.getBlock().getType() == Material.WATER) {
-            	player.sendMessage("[DEBUG] Nonsolid found. Getting new location...");
+            	player.sendMessage("[DEBUG] Water found. Getting new location...");
         		locPlusPlus = getNewLocation(player);
         	}
         		return locPlusPlus;
         case 2:
+        	World worldPM = locPlusMin.getWorld();
+        	worldPM.getChunkAt((int)locPlusMin.getX(), (int)locPlusMin.getZ()).load();
         	while(locPlusMin.getBlock().getType() == Material.WATER) {
-            	player.sendMessage("[DEBUG] Nonsolid found. Getting new location...");
+            	player.sendMessage("[DEBUG] Water found. Getting new location...");
         		locPlusMin = getNewLocation(player);
         	}
         		return locPlusMin;
         case 3:
+        	World worldMP = locMinPlus.getWorld();
+        	worldMP.getChunkAt((int)locMinPlus.getX(), (int)locMinPlus.getZ()).load();
         	while(locMinPlus.getBlock().getType() == Material.WATER) {
-            	player.sendMessage("[DEBUG] Nonsolid found. Getting new location...");
+            	player.sendMessage("[DEBUG] Water found. Getting new location...");
         		locMinPlus = getNewLocation(player);
         	}
         		return locMinPlus;
         case 4:
+        	World worldMM = locMinMin.getWorld();
+        	worldMM.getChunkAt((int)locMinMin.getX(), (int)locMinMin.getZ()).load();
         	while(locMinMin.getBlock().getType() == Material.WATER) {
-            	player.sendMessage("[DEBUG] Nonsolid found. Getting new location...");
+            	player.sendMessage("[DEBUG] Water found. Getting new location...");
         		locMinMin = getNewLocation(player);
         	}
         		return locMinMin;
         default:
+        	World worldPP2 = locPlusPlus.getWorld();
+        	worldPP2.getChunkAt((int)locPlusPlus.getX(), (int)locPlusPlus.getZ()).load();
         	while(locPlusPlus.getBlock().getType() == Material.WATER) {
-            	player.sendMessage("[DEBUG] Nonsolid found. Getting new location...");
+            	player.sendMessage("[DEBUG] Water found. Getting new location...");
         		locPlusPlus = getNewLocation(player);
         	}
         		return locPlusPlus;
@@ -68,7 +78,7 @@ public class commandRtp implements CommandExecutor {
             if(player.hasPermission("rtp.rtp")) {
                 Location tpLocation = getNewLocation(player); // Get teleport location
                 World world = player.getWorld(); // Get world the player is in
-                world.getChunkAt((int)tpLocation.getX(), (int)tpLocation.getZ()).load(); // Loads chunk that the player will teleport to
+                //world.getChunkAt((int)tpLocation.getX(), (int)tpLocation.getZ()).load(); // Loads chunk that the player will teleport to
                 player.setVelocity(new Vector(0, 0, 0)); // Prevent fall damage
                 player.teleport(tpLocation); // Teleport player
                 player.sendMessage("\2478[\247bRTP\2478] \247aYou have been teleported!"); // Notify player that they have teleported
